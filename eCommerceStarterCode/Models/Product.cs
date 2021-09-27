84a5d4e8e7ed23fb147905bbeb5a9107abddf022
Seyfe74 <seyfeminas@gmail.com>
2021-09-27 14:09:32 -0700

    Product class created

---
+++ eCommerceStarterCode/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Product 
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Category { get; set; }
+
+
+    }
+}
+
+   
+

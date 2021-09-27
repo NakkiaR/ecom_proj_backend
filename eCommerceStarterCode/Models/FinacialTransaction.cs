48fcd77153d6b1ba2162afcc0f80d628b8d8e5c4
taylor herbots <therbots@live.com>
2021-09-27 16:45:38 -0500

    created finacialtransaction class

---
+++ eCommerceStarterCode/Models/FinacialTransaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class FinacialTransaction
+    {
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+        public int PreviousTransactionId { get; set; }
+        public string TransactionTypeCode { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int TransactionAmount { get; set; }
+        public string TransactionComment { get; set; }
+    }
+}

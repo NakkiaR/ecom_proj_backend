e85fe6718a7f75fd167c7aa3649bd79e1e38cea2
taylor herbots <therbots@live.com>
2021-09-27 16:40:56 -0500

    created orders class

---
+++ eCommerceStarterCode/Models/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Orders
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderAmount { get; set; }
+    }
+}

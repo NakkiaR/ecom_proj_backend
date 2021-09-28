53ff73347477dea69734983903412bba7525bb53
Your Name <macm91.mc@gmail.com>
2021-09-28 16:30:55 -0400

    merging

--- eCommerceStarterCode/Data/ApplicationDbContext.cs
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using eCommerceStarterCode.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace eCommerceStarterCode.Data
 {
     public class ApplicationDbContext : IdentityDbContext
@@ -13,14 +14,14 @@ namespace eCommerceStarterCode.Data
         }
 
         public DbSet<User> Users { get; set; }
-        public DBSet<Account> Accounts { get; set; }
-        public DBSet<Category> Categories { get; set; }
-        public DBSet<FinancialTransaction> FinancialTransactions { get; set; }
-        public DBSet<Order> Orders { get; set; }
-        public DBSet<PaymentMethod> PaymentMethods { get; set; }
-        public DBSet<Product> Products { get; set; }
-        public DBSet<Shipment> Shipments { get; set; }
-        public DBSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<FinancialTransaction> FinancialTransactions { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Shipment> Shipments { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)

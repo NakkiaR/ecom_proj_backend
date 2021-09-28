1d80abfc1e11a2cc6eac672b0b087ddd4f6465d9
Vernon Roberts <nakkiasr@live.com>
2021-09-28 12:53:02 -0700

    Added ForeignKeys to order.cs.

--- eCommerceStarterCode/Models/Order.cs
+++ eCommerceStarterCode/Models/Order.cs
@@ -8,9 +8,15 @@ namespace eCommerceStarterCode.Models
     public class Order
     {
         public int OrderId { get; set; }
-        public int UserId { get; set; }
-        public int ProductId { get; set; }
         public DateTime OrderDate { get; set; }
         public int OrderAmount { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }

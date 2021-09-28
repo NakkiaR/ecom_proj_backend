1a8ee7ae73c00402f476c5f168c957885b8239b0
Vernon Roberts <nakkiasr@live.com>
2021-09-28 12:47:53 -0700

    Added foreignkey to financialtransaction.cs

--- eCommerceStarterCode/Models/FinacialTransaction.cs
+++ eCommerceStarterCode/Models/FinacialTransaction.cs
@@ -8,11 +8,15 @@ namespace eCommerceStarterCode.Models
     public class FinacialTransaction
     {
         public int TransactionId { get; set; }
-        public int AccountId { get; set; }
+        
         public int PreviousTransactionId { get; set; }
         public string TransactionTypeCode { get; set; }
         public DateTime TransactionDate { get; set; }
         public int TransactionAmount { get; set; }
         public string TransactionComment { get; set; }
+
+        [ForeignKey("Account")]
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
     }
 }

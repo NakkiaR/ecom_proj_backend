2240b210934ce797384eb38d5171cd4d25606c2a
taylor herbots <therbots@live.com>
2021-09-28 21:43:33 -0500

    created shipmentCOntroller.cs and get and post endpoints

--- eCommerceStarterCode/Controllers/ShipmentController.cs
+++ eCommerceStarterCode/Controllers/ShipmentController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class ShipmentController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class ShipmentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public ShipmentController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public ShipmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpGet("Shipment"), Authorize]
-//        public IActionResult GetCurrentShipment()
-//        {
-//            var shipmentId = User.FindFirstValue("Id");
-//            var shipment = _context.Users.Find(shipmentId);
-//            if (shipment == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(shipment);
-//        }
+        [HttpGet("Shipment"), Authorize]
+        public IActionResult GetCurrentShipment()
+        {
+            var shipmentId = User.FindFirstValue("Id");
+            var shipment = _context.Users.Find(shipmentId);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+            return Ok(shipment);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody] Shipment value)
-//        {
-//            _context.Shipments.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] Shipment value)
+        {
+            _context.Shipments.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}

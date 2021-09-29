996f3fdb00886ab76cb0157068a79d078c090197
taylor herbots <therbots@live.com>
2021-09-28 21:42:52 -0500

    created reviewcontroller and get and post endpoints

--- eCommerceStarterCode/Controllers/ReviewController.cs
+++ eCommerceStarterCode/Controllers/ReviewController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class ReviewController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public ReviewController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public ReviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpGet("Review"), Authorize]
-//        public IActionResult GetCurrentReview()
-//        {
-//            var reviewId = User.FindFirstValue("Id");
-//            var review = _context.Users.Find(reviewId);
-//            if (review == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(review);
-//        }
+        [HttpGet("Review"), Authorize]
+        public IActionResult GetCurrentReview()
+        {
+            var reviewId = User.FindFirstValue("Id");
+            var review = _context.Users.Find(reviewId);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return Ok(review);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody] Review value)
-//        {
-//            _context.Reviews.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] Review value)
+        {
+            _context.Reviews.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}

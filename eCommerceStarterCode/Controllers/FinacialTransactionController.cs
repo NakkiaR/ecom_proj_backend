32d0d94022bb103befa93debf818183bbc353329
taylor herbots <therbots@live.com>
2021-09-28 21:40:22 -0500

    created financialtransaction controller.cs and get and post end points

--- eCommerceStarterCode/Controllers/FinacialTransactionController.cs
+++ eCommerceStarterCode/Controllers/FinacialTransactionController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class FinacialTransactionController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class FinacialTransactionController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public FinacialTransactionController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public FinacialTransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpGet("FinacialTransaction"), Authorize]
-//        public IActionResult GetCurrentFinancialTransaction()
-//        {
-//            var financialTransactionId = User.FindFirstValue("Id");
-//            var financialTransaction = _context.Users.Find(financialTransactionId);
-//            if (financialTransaction == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(financialTransaction);
-//        }
+        [HttpGet("FinacialTransaction"), Authorize]
+        public IActionResult GetCurrentFinancialTransaction()
+        {
+            var financialTransactionId = User.FindFirstValue("Id");
+            var financialTransaction = _context.Users.Find(financialTransactionId);
+            if (financialTransaction == null)
+            {
+                return NotFound();
+            }
+            return Ok(financialTransaction);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody] FinancialTransaction value)
-//        {
-//            _context.FinancialTransactions.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] FinancialTransaction value)
+        {
+            _context.FinancialTransactions.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}

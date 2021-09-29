e7cdc0603db056d9576bd1a3a268fb56cc9bfd7f
taylor herbots <therbots@live.com>
2021-09-28 21:41:02 -0500

    created ordercontroller.cs and get and post endpoints

--- eCommerceStarterCode/Controllers/OrderController.cs
+++ eCommerceStarterCode/Controllers/OrderController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class OrderController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public OrderController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpGet("Order"), Authorize]
-//        public IActionResult GetCurrentOrder()
-//        {
-//            var orderId = User.FindFirstValue("Id");
-//            var order = _context.Users.Find(orderId);
-//            if (order == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(order);
-//        }
+        [HttpGet("Order"), Authorize]
+        public IActionResult GetCurrentOrder()
+        {
+            var orderId = User.FindFirstValue("Id");
+            var order = _context.Users.Find(orderId);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody] Order value)
-//        {
-//            _context.Orders.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] Order value)
+        {
+            _context.Orders.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}

10affb42914b72fdc9ede51703c1f9672d476ca7
taylor herbots <therbots@live.com>
2021-09-28 21:41:38 -0500

    created payment methodcontrolloer.cs and get and post endpoints

--- eCommerceStarterCode/Controllers/PaymentMethodController.cs
+++ eCommerceStarterCode/Controllers/PaymentMethodController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class PaymentMethodController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class PaymentMethodController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public PaymentMethodController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public PaymentMethodController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpGet("PaymentMethod"), Authorize]
-//        public IActionResult GetCurrentPaymentMethod()
-//        {
-//            var paymentMethodId = User.FindFirstValue("Id");
-//            var paymentMethod = _context.Users.Find(paymentMethodId);
-//            if (paymentMethod == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(paymentMethod);
-//        }
+        [HttpGet("PaymentMethod"), Authorize]
+        public IActionResult GetCurrentPaymentMethod()
+        {
+            var paymentMethodId = User.FindFirstValue("Id");
+            var paymentMethod = _context.Users.Find(paymentMethodId);
+            if (paymentMethod == null)
+            {
+                return NotFound();
+            }
+            return Ok(paymentMethod);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody] PaymentMethod value)
-//        {
-//            _context.PaymentMethods.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] PaymentMethod value)
+        {
+            _context.PaymentMethods.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}

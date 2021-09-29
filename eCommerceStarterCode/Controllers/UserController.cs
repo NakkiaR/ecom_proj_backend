d366677fa7913bcde9ca1a0f378b12be74833ce8
taylor herbots <therbots@live.com>
2021-09-28 21:44:47 -0500

    created usercontroller.cs and get and post endpoints

--- eCommerceStarterCode/Controllers/UserController.cs
+++ eCommerceStarterCode/Controllers/UserController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class UserController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
-//        public UserController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
-//        // <baseurl>/api/examples/user
-//        [HttpGet("user"), Authorize]
-//        public IActionResult GetCurrentUser()
-//        {
-//            var userId = User.FindFirstValue("id");
-//            var user = _context.Users.Find(userId);
-//            if(user == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(user);
-//        }
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // <baseurl>/api/examples/user
+        [HttpGet("user"), Authorize]
+        public IActionResult GetCurrentUser()
+        {
+            var userId = User.FindFirstValue("id");
+            var user = _context.Users.Find(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody]User value)
-//        {
-//            _context.Users.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] User value)
+        {
+            _context.Users.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}

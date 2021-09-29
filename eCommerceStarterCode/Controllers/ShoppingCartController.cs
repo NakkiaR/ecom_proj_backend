4f1d0b90d717a0bf1f86f3a7ce6cc3a15a119730
taylor herbots <therbots@live.com>
2021-09-28 21:44:12 -0500

    created shoppingcartcontroller.cs and get and post endpoints

--- eCommerceStarterCode/Controllers/ShoppingCartController.cs
+++ eCommerceStarterCode/Controllers/ShoppingCartController.cs
@@ -1,45 +1,45 @@
-﻿//using eCommerceStarterCode.Data;
-//using eCommerceStarterCode.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-//namespace eCommerceStarterCode.Controllers
-//{
-//    [Route("api/examples")]
-//    [ApiController]
-//    public class ShoppingCartController : ControllerBase
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/examples")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public ShoppingCartController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public ShoppingCartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpGet("ShoppingCart"), Authorize]
-//        public IActionResult GetCurrentShoppingCart()
-//        {
-//            var shoppingCartId = User.FindFirstValue("Id");
-//            var shoppingCart = _context.Users.Find(shoppingCartId);
-//            if(shoppingCart == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(shoppingCart);
-//        }
+        [HttpGet("ShoppingCart"), Authorize]
+        public IActionResult GetCurrentShoppingCart()
+        {
+            var shoppingCartId = User.FindFirstValue("Id");
+            var shoppingCart = _context.Users.Find(shoppingCartId);
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+            return Ok(shoppingCart);
+        }
 
-//        [HttpPost]
+        [HttpPost]
 
-//        public IActionResult Post([FromBody]ShoppingCart value)
-//        {
-//            _context.ShoppingCarts.Add(value);
-//            _context.SaveChanges();
-//            return StatusCode(201, value);
-//        }
-//    }
-//}
+        public IActionResult Post([FromBody] ShoppingCart value)
+        {
+            _context.ShoppingCarts.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}
